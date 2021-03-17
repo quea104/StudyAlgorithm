0132df7914f03788fc88e7ee74ca0272040aa57d
dasom <gurre104@gmail.com>
2021-03-17 14:23:52 +0900

    Create default package > Main class

---
+++ StudyAlgorithm/src/Main.java
@@ -0,0 +1,13 @@
+import packStudyAlgorithm.NQueen;
+
+public class Main {
+	public static void main(String[] args) {
+//		NewId classNewId = new NewId();
+//		String answerNewId = classNewId.solution(".1.");
+//		System.out.println("Answer:"+answerNewId);
+		
+		NQueen classNQueen = new NQueen();
+		int answerNQueen = classNQueen.solution(3);
+		System.out.println("Answer:"+answerNQueen);
+	}
+}
