b762b1dc670879bf9bd9637a18d9b86f46e99a74
mikerri <gurre104@gmail.com>
2021-06-15 15:05:44 +0900

    Commit

--- StudyAlgorithm/src/TestBackJoon/T18870CompressionCoordinates.java
+++ StudyAlgorithm/src/TestBackJoon/T18870CompressionCoordinates.java
@@ -14,31 +14,75 @@ package TestBackJoon;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.HashMap;
 public class T18870CompressionCoordinates {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		ArrayList<Integer> list = new ArrayList<>();
+		
+		int[] arr = new int[N];
 		for(int i = 0; i < N; i++) {
-			list.add(Integer.parseInt(st.nextToken()));
+			arr[i] = Integer.parseInt(st.nextToken());			
 		}
 		
-		int index = 0;
-		ArrayList<Integer> indexList = new ArrayList<>();
-		list.sort((e1, e2) -> {
-			if(e1 > e2) {
-				indexList.add(index++);
-			}
-		});
-
+		int[] sortArr = arr.clone();
+		Arrays.sort(sortArr);
 		
+		HashMap<Integer, Integer> map = new HashMap<>();
+		int index = 0;
+		for(int n : sortArr) {
+			if(!map.containsKey(n)) {
+				map.put(n, index++);
+			}
+		}
+
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < N; i++) {
-			sb.append(list.get(i)).append(" ");	
+		for(int n : arr) {
+			sb.append(map.get(n)).append(" ");
 		}
 		System.out.println(sb.toString());
+		
+		/*
+		ArrayList<Integer> list = new ArrayList<>();
+		ArrayList<ArrayList<Integer>> cList = new ArrayList<ArrayList<Integer>>();
+		
+		for(int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			
+			list.add(num);
+			
+			ArrayList<Integer> innerList = new ArrayList<Integer>();
+			innerList.add(num);
+			innerList.add(-1);
+			cList.add(innerList);
+		}
+		
+		Collections.sort(list);
+
+		int index = 0;
+		int repeatNumber = 1000000001;
+		for(int i = 0; i < N; i++) {
+			int num = list.get(i);
+			if(repeatNumber != num) {
+				for(ArrayList<Integer> innerList : cList) {
+					if(innerList.get(1) == -1 && innerList.get(0) == num) {
+						innerList.set(1, index);
+					}
+				}
+				
+				index++;
+				repeatNumber = num;
+			}	
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(ArrayList<Integer> innerList : cList) {
+			sb.append(innerList.get(1)).append(" ");	
+		}
+		System.out.println(sb.toString());
+		*/
 	}
 }
