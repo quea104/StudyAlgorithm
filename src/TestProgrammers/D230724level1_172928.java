ea31e1c042c859fe8c9a363bb3ce7df4a5688a75
MyQuestion <quea104@naver.com>
2023-07-25 10:39:51 +0900

    commit

---
+++ src/TestProgrammers/D230724level1_172928.java
@@ -0,0 +1,29 @@
+package TestProgrammers;
+
+/*
+ * 문제명: 신고 결과 받기
+ * 일자: 23.07.24.월
+ * https://school.programmers.co.kr/learn/courses/30/lessons/172928
+ * 문제 설명
+
+ */
+
+public class D230724level1_172928 {
+    public static void main() {
+        String[] park = {"SOO","OOO","OOO"}; // 사용자 ID 목록
+        String[] routes = {"E 2","S 2","W 1"}; // 신고 내용 목록
+
+        int[] answer = solution(park, routes);
+
+        for(int i = 0; i < answer.length; i++) {
+            System.out.println(answer[i]);
+        }
+    }
+
+    static int[] solution(String[] park, String[] routes) {
+        int[] answer = {};
+
+
+        return answer;
+    }
+}
